e3ac896a527bd8026c4576b7cdd6c65c757e7334
tterrag1098 <tterrag1098@gmail.com>
2016-10-07 15:45:31 -0400

    Remove pointless methods from error handler

--- src/wci/frontend/subsetc/SubsetCErrorHandler.java
+++ src/wci/frontend/subsetc/SubsetCErrorHandler.java
@@ -67,42 +67,4 @@ public class SubsetCErrorHandler
                                                      fatalText}));
         System.exit(errorCode.getStatus());
     }
-    
-    /**
-     * Flag an error in the source line.
-     * @param token the bad token.
-     * @param errorCode the error code.
-     * @param parser the parser.
-     * @return the flagger string.
-     */
-    public void flag(Token token, PascalErrorCode errorCode, Parser parser)
-    {
-        // Notify the parser's listeners.
-        parser.sendMessage(new Message(SYNTAX_ERROR,
-                                       new Object[] {token.getLineNumber(),
-                                                     token.getPosition(),
-                                                     token.getText(),
-                                                     errorCode.toString()}));
-
-        if (++errorCount > MAX_ERRORS) {
-            abortTranslation(TOO_MANY_ERRORS, parser);
-        }
-    }
-    
-    /**
-     * Abort the translation.
-     * @param errorCode the error code.
-     * @param parser the parser.
-     */
-    public void abortTranslation(PascalErrorCode errorCode, Parser parser)
-    {
-        // Notify the parser's listeners and then abort.
-        String fatalText = "FATAL ERROR: " + errorCode.toString();
-        parser.sendMessage(new Message(SYNTAX_ERROR,
-                                       new Object[] {0,
-                                                     0,
-                                                     "",
-                                                     fatalText}));
-        System.exit(errorCode.getStatus());
-    }
 }
